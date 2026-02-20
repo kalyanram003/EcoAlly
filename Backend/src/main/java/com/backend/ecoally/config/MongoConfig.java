59b5337ea412b19c8dfaf689980d5fae21f7c18e
kalyanram003 <kalyanram2053@gmail.com>
2026-02-20 10:47:33 +0530

    configs created

---
+++ Backend/src/main/java/com/backend/ecoally/config/MongoConfig.java
@@ -0,0 +1,9 @@
+package com.ecolearn.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
